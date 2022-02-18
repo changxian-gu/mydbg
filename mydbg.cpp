99601af7c751285f001d4864f9ee3c541438d71c
changxian-gu <changxian_gu@stu.xidian.edu.cn>
2022-02-18 20:49:25 +0800

    v2

--- mydbg.cpp
+++ mydbg.cpp
@@ -7,31 +7,39 @@
 #include <unistd.h>
 #include <stdint.h>
 
-void execute_cmd(pid_t pid, char* line);
-void execute_continue(pid_t pid);
-void start_debug(pid_t pid);
+class Debugger {
+private:
+	pid_t pid;
+public:
+	Debugger(pid_t pid);
+	void debug();
+	void execute_cmd(char* line);
+	void execute_continue();
+};
 
-void start_debug(pid_t pid) {
+Debugger::Debugger(pid_t pid):pid(pid) {}
+
+void Debugger::debug() {
 	int wait_status;
 	waitpid(pid, &wait_status, 0);
 
 	char* line = NULL;
 	while ((line = linenoise("mydbg> ")) != NULL) {
-		execute_cmd(pid, line);
+		execute_cmd(line);
 		linenoiseHistoryAdd(line);
 		linenoiseFree(line);
 	}
 }
 
-void execute_cmd(pid_t pid, char* line) {
+void Debugger::execute_cmd(char* line) {
 	if (strcmp(line, "continue") == 0) {
-		execute_continue(pid);
+		execute_continue();
 	} else {
 		printf("No such command!!!\n");
 	}			
 }
 
-void execute_continue(pid_t pid) {
+void Debugger::execute_continue() {
 	ptrace(PTRACE_CONT, pid, NULL, NULL);
 	int wait_status;
 	waitpid(pid, &wait_status, 0);
@@ -82,6 +90,7 @@ int main(int argc, char* argv[]){
 		ptrace(PTRACE_TRACEME, 0, NULL, NULL);
 		execl(prog, prog, NULL);
 	} else if (pid >= 1) {
-		start_debug(pid);
+		Debugger dbg(pid);
+		dbg.debug();
 	}
 }
